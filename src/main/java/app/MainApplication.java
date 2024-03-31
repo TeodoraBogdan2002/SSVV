a0b77ce1c737b1d2d27129da46ef448a18b8e5f6
Bogdan-Teodora <teodorabogdan2002@gmail.com>
2024-03-31 16:12:05 +0300

    .

---
+++ src/main/java/app/MainApplication.java
@@ -0,0 +1,38 @@
+package main.java.app;
+
+
+import main.java.repository.StudentXMLRepo;
+import main.java.repository.TemaXMLRepo;
+import main.java.repository.NotaXMLRepo;
+import main.java.service.Service;
+import main.java.validation.NotaValidator;
+import main.java.validation.StudentValidator;
+import main.java.validation.TemaValidator;
+import main.java.view.UI;
+
+
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+       //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        UI ui = new UI(service);
+        ui.run();
+    }
+
+}
